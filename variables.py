d92d2d4728dfe97d24c45f02714d85119fd3a61f
3picF4iL <haskypl1@gmail.com>
2020-12-09 00:53:28 +0100

    All adjustable variables

---
+++ variables.py
@@ -0,0 +1,28 @@
+
+########################
+# Buildings
+########################
+
+building_message = "In this section you can build available constructions"
+
+buildings = [{'Name': 'Mine', 'met_req': '500', 'crist_req': '200', 'deu_req': '100'},
+             {'Name': 'Solar', 'met_req': '500', 'crist_req': '200', 'deu_req': '100'},
+             {'Name': 'Atom', 'met_req': '500', 'crist_req': '200', 'deu_req': '100'},
+             {'Name': 'Wind', 'met_req': '23', 'crist_req': '123', 'deu_req': '23'},
+             {'Name': 'Tele', 'met_req': '500', 'crist_req': '200', 'deu_req': '100'},
+             {'Name': 'Geo', 'met_req': '500', 'crist_req': '200', 'deu_req': '23'},
+             ]
+
+########################
+# Technologies
+########################
+
+technologies_message = "Keep research new technologies for upgrading your imperio!"
+
+technologies = [{'Name': 'Impulse', 'met_req': '400', 'crist_req': '1200', 'deu_req': '1000'},
+             {'Name': 'Simple Engine', 'met_req': '1500', 'crist_req': '100', 'deu_req': '1200'},
+             {'Name': 'Quantum Computers', 'met_req': '500', 'crist_req': '2200', 'deu_req': '400'},
+             {'Name': 'Teleport', 'met_req': '11000', 'crist_req': '2000', 'deu_req': '1000'},
+             {'Name': 'Nuke', 'met_req': '10000', 'crist_req': '10000', 'deu_req': '1000'},
+             {'Name': 'Geo', 'met_req': '2500', 'crist_req': '1000', 'deu_req': '1000'},
+             ]
