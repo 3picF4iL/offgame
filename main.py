5224196081fe0413cb3ef1f0b8d702474513c1b3
Godzwon, KamilX <kamilx.godzwon@intel.com>
2021-04-26 09:46:52 +0200

    Some minor changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from panels import *
 
 class MainFrame(Frame):
     def __init__(self):
-        super().__init__(parent=None, title="Offgame", size=(600, 400))
+        super().__init__(parent=None, title="Offgame", size=(800, 600))
         panel = MainPanel(self)
         self.Show()
 
